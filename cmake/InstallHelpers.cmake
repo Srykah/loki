62949ee9cc2c7787c65eecd11e83be3989421a26
Thomas Bécavin <srykah@gmail.com>
2021-03-25 09:06:25 +0100

    [Feat] Automatic All.hpp header

--- cmake/InstallHelpers.cmake
+++ cmake/InstallHelpers.cmake
@@ -20,10 +20,19 @@ function(loki_target_install_files target)
     endforeach()
 endfunction()
 
+# Configuring All includes header
+function(loki_configure_all_includes_header)
+    cmake_parse_arguments(LIB "" "" "HEADERS" ${ARGN})
+    list(TRANSFORM LIB_HEADERS REPLACE ".+" "#include \"\\0\"" OUTPUT_VARIABLE LIB_INCLUDES)
+    list(JOIN LIB_INCLUDES "\n" LIB_ALL_INCLUDES)
+    configure_file(${PROJECT_SOURCE_DIR}/cmake/All.hpp.in ${CMAKE_CURRENT_SOURCE_DIR}/All.hpp @ONLY)
+endfunction()
+
 # Complete lib creation helper
 function(loki_create_lib)
-    cmake_parse_arguments(LIB "" "NAME" "HEADERS;SOURCES;PRIVATE_DEPS;PUBLIC_DEPS;INTERFACE_DEPS" ${ARGN})
-    add_library(${LIB_NAME} ${LIB_HEADERS} ${LIB_SOURCES})
+    cmake_parse_arguments(LIB "" "NAME" "HEADERS;INTERNAL_HEADERS;SOURCES;PRIVATE_DEPS;PUBLIC_DEPS;INTERFACE_DEPS" ${ARGN})
+    loki_configure_all_includes_header(HEADERS ${LIB_HEADERS})
+    add_library(${LIB_NAME} ${LIB_HEADERS} ${LIB_INTERNAL_HEADERS} ${LIB_SOURCES} All.hpp)
     add_library(loki::${LIB_NAME} ALIAS ${LIB_NAME})
     target_link_libraries(${LIB_NAME}
             PRIVATE "${LIB_PRIVATE_DEPS}"
@@ -33,6 +42,6 @@ function(loki_create_lib)
             $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/src>
             $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>)
     loki_add_lib_prefix(${LIB_NAME})
-    loki_target_install_files(${LIB_NAME} ${LIB_HEADERS})
+    loki_target_install_files(${LIB_NAME} ${LIB_HEADERS} ${LIB_INTERNAL_HEADERS} All.hpp)
     install(TARGETS ${LIB_NAME} EXPORT loki_targets)
 endfunction()
