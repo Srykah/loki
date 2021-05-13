30a8d5ace6da59593d691ba56ff62e681df943e7
Thomas Bécavin <srykah@gmail.com>
2021-05-13 17:16:29 +0200

    [Feat] Added simple text templating (#16)
    Added TemplateEngine, with functions to add a simple value or a lambda to dynamically query the template argument.

---
+++ src/loki/tpl/impl/TemplateEngine.hxx
@@ -0,0 +1,20 @@
+/*!
+ * \file TemplateEngine.hxx
+ * \author Srykah
+ * \copyright GNU GPL v3.0
+ */
+#pragma once
+
+namespace loki::tpl {
+
+template <class T>
+void TemplateEngine::addRef(const std::string& name, const T& t) {
+  refs.insert_or_assign(name, [&t]() { return std::to_string(t); });
+}
+
+template <class T>
+void TemplateEngine::addPtr(const std::string& name, const T* t) {
+  refs.insert_or_assign(name, [t]() { return std::to_string(*t); });
+}
+
+}
