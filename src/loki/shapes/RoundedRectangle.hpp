cb9dec80228b846dc20798824ae1e541cf5c9b3e
Thomas Bécavin <srykah@gmail.com>
2021-06-06 12:53:28 +0200

    [Feat] Added Rounded Rectangle

--- src/loki/shapes/RoundedRectangle.hpp
+++ src/loki/shapes/RoundedRectangle.hpp
@@ -1,5 +1,5 @@
 /*!
- * \file RoundedRectangle.hpp.h
+ * \file RoundedRectangle.hpp
  * \author Srykah
  * \copyright GNU GPL v3.0
  */
@@ -10,8 +10,21 @@
 namespace loki::shapes {
 
 class RoundedRectangle : public sf::Shape {
+ public:
+  explicit RoundedRectangle(sf::Vector2f size,
+                            float borderRadius,
+                            std::size_t borderPointCount = 7);
+  ~RoundedRectangle() override = default;
+  std::size_t getPointCount() const override;
+  sf::Vector2f getPoint(std::size_t index) const override;
 
+ private:
+  sf::Vector2f getQuadrantOrigin(std::size_t quadrant) const;
 
+ private:
+  sf::Vector2f size;
+  float borderRadius;
+  std::size_t borderPointCount;
 };
 
-}
+}  // namespace loki::shapes
