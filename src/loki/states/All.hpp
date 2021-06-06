6e42369de671b7352d31af00e7d9d6417da74d10
Thomas Bécavin <srykah@gmail.com>
2021-06-06 10:38:27 +0200

    [Feat] Added simple state machine (#17)

---
+++ src/loki/states/All.hpp
@@ -0,0 +1,11 @@
+/*!
+ * \file All.hpp
+ * \author Srykah
+ * \copyright GNU GPL v3.0
+ */
+#pragma once
+
+#include "State.hpp"
+#include "StateIndex.hpp"
+#include "StateMachine.hpp"
+#include "Transition.hpp"
---
+++ src/loki/states/Transition.hpp
@@ -0,0 +1,39 @@
+/*!
+ * \file Transition.hpp.h
+ * \author Srykah
+ * \copyright GNU GPL v3.0
+ */
+#pragma once
+
+#include <functional>
+#include <utility>
+#include "StateIndex.hpp"
+
+namespace loki::states {
+
+struct Transition {
+  Transition(StateIndex from,
+             StateIndex to,
+             std::function<bool()>  isActive,
+             std::function<void()>  onTrigger = nullptr)
+      : from(from),
+        to(to),
+        isActive(std::move(isActive)),
+        onTrigger(std::move(onTrigger)) {}
+
+  template <class Enum, std::enable_if_t<std::is_enum_v<Enum>, bool> = true>
+  Transition(Enum from,
+             Enum to,
+             const std::function<bool()>& isActive,
+             const std::function<void()>& onTrigger = nullptr)
+      : Transition(static_cast<StateIndex>(from),
+                   static_cast<StateIndex>(to),
+                   isActive,
+                   onTrigger) {}
+
+  StateIndex from, to;
+  std::function<bool()> isActive;
+  std::function<void()> onTrigger;
+};
+
+}  // namespace loki::states
