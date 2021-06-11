4ec1e08ba26e6fc1dee3bbd7d6b8f95bdcfc549e
Thomas Bécavin <srykah@gmail.com>
2021-06-11 21:33:51 +0200

    [Feat] Moved Interpolate to math module, added LinAlg2D

---
+++ src/loki/math/All.hpp
@@ -0,0 +1,9 @@
+/*!
+ * \file All.hpp
+ * \author Srykah
+ * \copyright GNU GPL v3.0
+ */
+#pragma once
+
+#include "Interpolate.hpp"
+#include "LinAlg2D.hpp"
---
+++ src/loki/math/LinAlg2D.cpp
@@ -0,0 +1,12 @@
+/*!
+ * \file LinAlg2D.cpp.c
+ * \author Srykah
+ * \copyright GNU GPL v3.0
+ */
+#include "LinAlg2D.hpp"
+
+namespace loki::math {
+
+
+
+}
