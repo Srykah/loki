b03e57de9253bae4dd8f5fa10e722d20db914fdf
Thomas Bécavin <srykah@gmail.com>
2021-07-14 16:34:08 +0200

    [Fix] Keyframe json optional override (#24)

--- src/loki/anim/Keyframe.hpp
+++ src/loki/anim/Keyframe.hpp
@@ -19,6 +19,27 @@ struct Keyframe {
   std::optional<sf::IntRect> textureRect;
 };
 
-NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(Keyframe, origin, position, rotation, scale, color, textureRect)
+inline void to_json(nlohmann::json& nlohmann_json_j,
+                    const Keyframe& nlohmann_json_t) {
+  nlohmann_json_j["origin"] = nlohmann_json_t.origin;
+  nlohmann_json_j["position"] = nlohmann_json_t.position;
+  nlohmann_json_j["rotation"] = nlohmann_json_t.rotation;
+  nlohmann_json_j["scale"] = nlohmann_json_t.scale;
+  nlohmann_json_j["color"] = nlohmann_json_t.color;
+  nlohmann_json_j["textureRect"] = nlohmann_json_t.textureRect;
+}
+
+inline void from_json(const nlohmann::json& nlohmann_json_j,
+                      Keyframe& nlohmann_json_t) {
+#define LOKI_GET_OPTIONAL_FIELD(field) \
+  nlohmann_json_t.field = nlohmann_json_j.value<decltype(nlohmann_json_t.field)>(#field, std::nullopt)
+  LOKI_GET_OPTIONAL_FIELD(origin);
+  LOKI_GET_OPTIONAL_FIELD(position);
+  LOKI_GET_OPTIONAL_FIELD(rotation);
+  LOKI_GET_OPTIONAL_FIELD(scale);
+  LOKI_GET_OPTIONAL_FIELD(color);
+  LOKI_GET_OPTIONAL_FIELD(textureRect);
+#undef LOKI_GET_OPTIONAL_FIELD
+}
 
 }
